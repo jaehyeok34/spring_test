b596b53fde1764e59c32cec15fbb9a66d382a698
Jang JaeHyeok <hyeok9942@gmail.com>
2023-08-29 14:10:59 +0900

    0829 labtop

--- hello_spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello_spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,44 +1,44 @@
-package hello.hello_spring.controller;
+// package hello.hello_spring.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Controller
-public class HelloController {
+// @Controller
+// public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
-    }
+//     @GetMapping("hello")
+//     public String hello(Model model) {
+//         model.addAttribute("data", "hello!!");
+//         return "hello";
+//     }
 
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello-template";
-    }
+//     @GetMapping("hello-mvc")
+//     public String helloMvc(@RequestParam("name") String name, Model model) {
+//         model.addAttribute("name", name);
+//         return "hello-template";
+//     }
 
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloAPI(@RequestParam("name") String name) {
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
+//     @GetMapping("hello-api")
+//     @ResponseBody
+//     public Hello helloAPI(@RequestParam("name") String name) {
+//         Hello hello = new Hello();
+//         hello.setName(name);
+//         return hello;
+//     }
 
-    static class Hello {
-        private String name;
+//     static class Hello {
+//         private String name;
 
-        public String getName() {
-            return name + "test message";
-        }
+//         public String getName() {
+//             return name + "test message";
+//         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
-}
+//         public void setName(String name) {
+//             this.name = name;
+//         }
+//     }
+// }
